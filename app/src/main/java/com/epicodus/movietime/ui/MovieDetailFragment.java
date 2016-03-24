4d32468e19112b21da48eeeb81bb7ee14aed5e48
Jeffrey Ruder and Midori Bowen <midori.p.bowen@gmail.com>
2016-03-24 16:43:41 -0700

    Send sms to contact with invitation to movie title.

--- app/src/main/java/com/epicodus/movietime/ui/MovieDetailFragment.java
+++ app/src/main/java/com/epicodus/movietime/ui/MovieDetailFragment.java
@@ -1,9 +1,16 @@
 package com.epicodus.movietime.ui;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +21,8 @@ import android.widget.TextView;
 import com.epicodus.movietime.R;
 import com.squareup.picasso.Picasso;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import info.movito.themoviedbapi.model.MovieDb;
@@ -21,7 +30,7 @@ import info.movito.themoviedbapi.model.MovieDb;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MovieDetailFragment extends Fragment {
+public class MovieDetailFragment extends Fragment implements View.OnClickListener {
     @Bind(R.id.movieNameTextView) TextView mMovieNameTextView;
     @Bind(R.id.movieBackdropImageView) ImageView mMovieBackdropImageView;
     @Bind(R.id.releaseDateTextView) TextView mReleaseDateTextView;
@@ -30,6 +39,7 @@ public class MovieDetailFragment extends Fragment {
     @Bind(R.id.inviteFriendButton) Button mInviteFriendButton;
 
     private MovieDb mMovie;
+    private static final int PICK_CONTACT_REQUEST = 1;
 
     public static MovieDetailFragment newInstance(MovieDb movie) {
         MovieDetailFragment movieDetailFragment = new MovieDetailFragment();
@@ -65,7 +75,50 @@ public class MovieDetailFragment extends Fragment {
             mRatingTextView.setVisibility(View.INVISIBLE);
         }
         mOverviewTextView.setText(mMovie.getOverview());
+        mInviteFriendButton.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mInviteFriendButton) {
+            Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+            pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+            startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri contactUri = data.getData();
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                try {
+                    Cursor cursor = this.getContext().getContentResolver().query(contactUri, projection, null, null, null);
+                    cursor.moveToFirst();
+
+                    int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    String number = cursor.getString(column);
+                    cursor.close();
+                    sendSMS(this.getView(), number, mMovie);
+                } catch (NullPointerException npe) {
+                    npe.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public void sendSMS(View v, String number, MovieDb movie) {
+        String movieUrl = "https://www.themoviedb.org/movie/" + movie.getId();
+
+        Uri uri = Uri.parse("smsto:" + number);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        intent.putExtra("sms_body",
+                "Let's go see " + movie.getTitle() + "! " + movieUrl);
+        startActivity(intent);
     }
 
 }
