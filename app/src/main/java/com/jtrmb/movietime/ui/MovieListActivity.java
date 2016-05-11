5c66a5f6a1b34ff1910aed1939bee83dca05a8b5
Jeffrey Ruder <ruderjt@gmail.com>
2016-05-10 21:55:17 -0700

    Refactor MovieListActivity to use CardView

--- app/src/main/java/com/jtrmb/movietime/ui/MovieListActivity.java
+++ app/src/main/java/com/jtrmb/movietime/ui/MovieListActivity.java
@@ -4,13 +4,11 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Gravity;
-import android.widget.Toast;
 
 import com.jtrmb.movietime.R;
 import com.jtrmb.movietime.adapters.MovieListAdapter;
@@ -28,12 +26,13 @@ public class MovieListActivity extends AppCompatActivity {
     private MovieListAdapter mAdapter;
     private LinearLayoutManager mManager = new LinearLayoutManager(MovieListActivity.this);
     private ProgressDialog mLoadingProgressDialog;
-
+    
+    /* Member variables used to implement endless scroll */
     private int previousTotal = 0;
     private boolean isLoading = true;
     private int visibleThreshold = 5;
     int firstVisibleItem, visibleItemCount, totalItemCount;
-    int pageCounter = 1;
+    int currentPage = 1;
 
 
     @Override
@@ -42,16 +41,22 @@ public class MovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_list);
         ButterKnife.bind(this);
 
+        /* Gets the user's search query */
         Intent intent = getIntent();
         final String searchQuery = intent.getStringExtra("searchQuery");
-        TaskParams params = new TaskParams(searchQuery, pageCounter);
+
+        /* Shows loading progress dialog spinner */
         initializeProgressDialog();
         mLoadingProgressDialog.show();
 
+        /* Retrieves search results asynchronously so API call does not block UI thread */
+        TaskParams params = new TaskParams(searchQuery, currentPage);
         new GetMovieTask().execute(params);
+
         mRecyclerView.setLayoutManager(mManager);
         mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
+            /* Triggers a new GetMovieTask when the user scrolls to the end of the list */
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
 
@@ -65,32 +70,33 @@ public class MovieListActivity extends AppCompatActivity {
                         previousTotal = totalItemCount;
                     }
                 }
+
+                /* Checks to see if the end of the list is close and, if so, gets the next page of movies */
                 if (!isLoading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
-                    //need to get more movies
-                    pageCounter ++;
-                    TaskParams params = new TaskParams(searchQuery, pageCounter);
+                    currentPage++;
+                    TaskParams params = new TaskParams(searchQuery, currentPage);
                     new GetMovieTask().execute(params);
                     isLoading = true;
                 }
             }
         });
-
     }
 
+    /* Retrieves movie search results asynchronously and adds them to the RecyclerView adapter */
     private class GetMovieTask extends AsyncTask<TaskParams, Void, List<MovieDb>> {
         @Override
         protected List<MovieDb> doInBackground(TaskParams... params) {
+            /* Uses both the search query and the page number to retrieve the correct results */
             return SearchService.movieSearch(params[0].query, 0, params[0].page, getApplicationContext());
         }
 
         @Override
         protected void onPostExecute(List<MovieDb> result) {
-
             if (mAdapter != null) {
-                //not the first time getting movies, so add to the existing adapter
+                /* Adds the new results to the existing adapter, since this is not the first time retrieving movies */
                 mAdapter.addMovies(result);
             } else {
-                //first time getting movies
+                /* Creates a new adapter with the results, since this is the first time retrieving movies */
                 mLoadingProgressDialog.dismiss();
                 if (result.size() == 0) {
                     showFailureDialog("Please try another search.");
@@ -99,11 +105,10 @@ public class MovieListActivity extends AppCompatActivity {
                 mRecyclerView.setAdapter(mAdapter);
             }
             mRecyclerView.setHasFixedSize(true);
-            mRecyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), R.drawable.divider_shape));
         }
     }
 
-    //wrapper class for giving both query and page to GetMovieTask
+    /* Builds a TaskParams object to allow for passing both the search query and the page number to GetMovieTask */
     private static class TaskParams {
         String query;
         int page;
@@ -114,6 +119,7 @@ public class MovieListActivity extends AppCompatActivity {
         }
     }
 
+    /* Builds a loading progress dialog spinner so that the user knows movie information is being retrieved */
     private void initializeProgressDialog() {
         mLoadingProgressDialog = new ProgressDialog(this);
         mLoadingProgressDialog.setTitle(getString(R.string.searching));
@@ -121,6 +127,7 @@ public class MovieListActivity extends AppCompatActivity {
         mLoadingProgressDialog.setCancelable(false);
     }
 
+    /* Shows an alert dialog if the movie search finds no results */
     private void showFailureDialog(String message) {
         new AlertDialog.Builder(this)
                 .setTitle(getString(R.string.no_results))
@@ -128,6 +135,7 @@ public class MovieListActivity extends AppCompatActivity {
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        /* Transition to the MainActivity so the user can search again */
                         Intent intent = new Intent(MovieListActivity.this, MainActivity.class);
                         startActivity(intent);
                     }
